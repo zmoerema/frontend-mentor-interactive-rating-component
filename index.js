812110b5305a7b5a6c38378069d06516250d4b20
zoemoerema <zoemoeremans@yahoo.fr>
2024-02-29 00:13:37 +0100

    button events done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const ratingState = document.getElementById("rating-state");
-const thankYouState = document.getElementById("thank-you-state");
+const rootStyles = getComputedStyle(document.documentElement);
+
+const semiGrey = rootStyles.getPropertyValue('--Medium-Grey-Semi');
+const lightGrey = rootStyles.getPropertyValue('--Light-Grey');
+const grey = rootStyles.getPropertyValue('--Medium-Grey');
+const orange = rootStyles.getPropertyValue('--Orange');
+const white = rootStyles.getPropertyValue('--White');
 
 const submitButton = document.getElementById("submit");
 const allRates = document.getElementsByClassName("rate");
@@ -14,7 +19,33 @@ function showRate() {
     htmlRate.innerHTML = chosenRate;
 }
 
+function updateButtonColor(button) {
+    if (button.textContent === chosenRate) {
+        button.style.color = white;
+        button.style.backgroundColor = orange;
+    } else {
+        button.style.color = lightGrey;
+        button.style.backgroundColor = semiGrey;
+    }
+}
+
+function clickRateButton(value) {  
+    setRate(value);
+
+    Array.from(allRates).forEach(rate => {
+        updateButtonColor(rate);
+    });
+}
+
+function hoverRateButton(rate) {
+    rate.style.color = white;
+    rate.style.backgroundColor = grey;
+}
+
 function showThankYou() {
+    const ratingState = document.getElementById("rating-state");
+    const thankYouState = document.getElementById("thank-you-state");
+
     ratingState.classList.add('hidden');
     thankYouState.classList.remove('hidden');
 
@@ -22,11 +53,20 @@ function showThankYou() {
 }
 
 Array.from(allRates).forEach(rate => {
-    const value = rate.textContent;
-    rate.addEventListener('click', () => setRate(value));
+    rate.addEventListener('mouseover', () => hoverRateButton(rate));
+    rate.addEventListener('mouseout', () => updateButtonColor(rate));
+
+    rate.addEventListener('click', () => {
+        const value = rate.textContent;
+        clickRateButton(value);
+    });
 });
 
 submitButton.addEventListener('click', (event) => {
     event.preventDefault();
-    showThankYou();
+    if (!chosenRate) {
+        alert("Please select a rate before submitting.");
+    } else {
+        showThankYou();
+    }
 });
