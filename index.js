4fe910e323801d380ba9f02ae4b69e7c5ade9f85
zoemoerema <zoemoeremans@yahoo.fr>
2024-02-28 22:30:59 +0100

    submit done

---
+++ index.js
@@ -0,0 +1,14 @@
+let ratingState = document.getElementById("rating-state");
+let thankYouState = document.getElementById("thank-you-state");
+
+let ratingContainer = document.getElementById("rating-state.container");
+let thankYouContainer = document.getElementById("thank-you-state.container");
+
+let submitButton = document.getElementById("submit");
+
+function showThankYou() {
+    ratingState.classList.add('hidden');
+    thankYouState.classList.remove('hidden')
+}
+
+submitButton.addEventListener('click', () => showThankYou())
